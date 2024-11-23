eaada8870419f50a38eba5ebe8dcb1e53a32ef87
dmitry goremykin <dmitrygoremykinzw@gmail.com>
2024-11-23 19:33:56 +0200

    controllers for wl

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_21_142848) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_23_114029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_21_142848) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
+    t.text "overview"
     t.string "poster_url"
     t.float "rating"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+puts "Cleaning up database..."
+Movie.destroy_all
+
+puts "Fetching movies from TMDB..."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)["results"]
+
+puts "Creating movies..."
+movies.each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Created #{Movie.count} movies!"
